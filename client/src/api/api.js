914dcb78340691606a3b3b11747c86f34cf477ae
Sathish <duraisamysathish4@gmail.com>
2026-06-30 17:55:00 +0530

    add bk url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'https://photographystudio5.onrender.com';
 
 const api = axios.create({
   baseURL: `${BASE_URL}/api`,
